937f441b438ab1fa0dd49a745c0aeaea12b3589c
QbAnurag <anurag.patra@qbent.in>
2024-07-26 15:14:01 +0530

    formik yup setup

---
+++ src/component/FieldType.jsx
@@ -0,0 +1,276 @@
+/* eslint-disable react/prop-types */
+import { getIn } from "formik";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
+import { Calendar } from "primereact/calendar";
+import { RadioButton } from "primereact/radiobutton";
+import { MultiSelect } from "primereact/multiselect";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Password } from "primereact/password";
+import { Checkbox } from "primereact/checkbox";
+
+export const InputField = ({ field, form: { touched, errors }, ...props }) => {
+  return (
+    <div className="flex align-items-center justify-content-center">
+      {props.label && (
+        <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+          {props.label}{" "}
+          {props.requiredlabel === "true" && (
+            <span className="text-danger">*</span>
+          )}
+        </label>
+      )}
+      <InputText
+        id={field.name}
+        {...field}
+        {...props}
+        value={field.value ? field.value : ""}
+        className={`w-full mb-3  ${
+          Boolean(getIn(errors, field.name)) &&
+          getIn(touched, field.name) &&
+          "p-invalid"
+        }`}
+      />
+      {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+        <small className="text-danger">{getIn(errors, field.name)}</small>
+      )}
+    </div>
+  );
+};
+
+export const DropdownField = ({
+  field,
+  form: { touched, errors },
+  ...props
+}) => (
+  <div className="flex align-items-center justify-content-center">
+    {props.label && (
+      <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+        {props.label}{" "}
+        {props.requiredlabel === "true" && (
+          <span className="text-danger">*</span>
+        )}
+      </label>
+    )}
+    <Dropdown
+      id={field.name}
+      {...field}
+      {...props}
+      className={`w-full mb-3  ${
+        Boolean(getIn(errors, field.name)) &&
+        getIn(touched, field.name) &&
+        "p-invalid"
+      }`}
+    />
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const DateField = ({ field, form: { touched, errors }, ...props }) => (
+  <div className="flex align-items-center justify-content-center">
+    <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+      {props.label}{" "}
+      {props.requiredlabel === "true" && <span className="text-danger">*</span>}
+    </label>
+    <Calendar
+      id={field.name}
+      {...field}
+      {...props}
+      showIcon
+      className={`w-full mb-3  ${
+        Boolean(getIn(errors, field.name)) &&
+        getIn(touched, field.name) &&
+        "p-invalid"
+      }`}
+    />
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const TimeField = ({ field, form: { touched, errors }, ...props }) => (
+  <div className="flex align-items-center justify-content-center">
+    <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+      {props.label}{" "}
+      {props.requiredlabel === "true" && <span className="text-danger">*</span>}
+    </label>
+    <Calendar
+      id={field.name}
+      {...field}
+      {...props}
+      showIcon
+      timeOnly
+      // stepMinute={30}
+      icon={() => <i className="pi pi-clock" style={{ fontSize: "20px" }} />}
+      className={`w-full mb-3  ${
+        Boolean(getIn(errors, field.name)) &&
+        getIn(touched, field.name) &&
+        "p-invalid"
+      }`}
+    />
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const RadioField = ({
+  field,
+  form: { touched, errors, setFieldValue },
+  ...props
+}) => (
+  <div className="flex align-items-center justify-content-center">
+    <label
+      style={{ fontWeight: 500 }}
+      htmlFor={field.name}
+      className="block text-900 font-medium mb-2"
+    >
+      {props.label}{" "}
+      {props.requiredlabel === "true" && <span className="text-danger">*</span>}
+    </label>
+    <div className="flex flex-wrap gap-3">
+      {props.radiolist.map((item) => {
+        return (
+          <div className="flex align-items-center" key={item.id}>
+            <RadioButton
+              {...props}
+              {...field}
+              {...item}
+              inputId={item.id}
+              checked={field.value === item.value}
+              onChange={(e) => setFieldValue(field.name, e.value)}
+            />
+            <label htmlFor={item.id} className="ml-2">
+              {item.label}
+            </label>
+          </div>
+        );
+      })}
+    </div>
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const MultiDropdownField = ({
+  field,
+  form: { touched, errors },
+  ...props
+}) => (
+  <div className="flex align-items-center justify-content-center">
+    <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+      {props.label}{" "}
+      {props.requiredlabel === "true" && <span className="text-danger">*</span>}
+    </label>
+    <MultiSelect
+      id={field.name}
+      {...field}
+      {...props}
+      className={`w-full mb-3  ${
+        Boolean(getIn(errors, field.name)) &&
+        getIn(touched, field.name) &&
+        "p-invalid"
+      }`}
+      // maxSelectedLabels={3}
+    />
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const TextAreaInputField = ({
+  field,
+  form: { touched, errors },
+  ...props
+}) => (
+  <div className="flex align-items-center justify-content-center">
+    <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+      {props.label}{" "}
+      {props.requiredlabel === "true" && <span className="text-danger">*</span>}
+    </label>
+    <InputTextarea
+      id={field.name}
+      {...field}
+      {...props}
+      type="textarea"
+      value={field.value ? field.value : ""}
+      className={`w-full mb-3  ${
+        Boolean(getIn(errors, field.name)) &&
+        getIn(touched, field.name) &&
+        "p-invalid"
+      }`}
+    />
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const PasswordFiled = ({
+  field,
+  form: { touched, errors },
+  ...props
+}) => (
+  <div className="flex align-items-center justify-content-center">
+    {props.label && (
+      <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+        {props.label}{" "}
+        {props.requiredlabel === "true" && (
+          <span className="text-danger">*</span>
+        )}
+      </label>
+    )}
+
+    <Password
+      id={field.name}
+      {...field}
+      {...props}
+      style={{ width: "100%" }}
+      value={field.value ? field.value : ""}
+      className={`w-full mb-3  ${
+        Boolean(getIn(errors, field.name)) &&
+        getIn(touched, field.name) &&
+        "p-invalid"
+      }`}
+      toggleMask
+    />
+
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
+
+export const CheckField = ({
+  field,
+  form: { touched, errors, setFieldValue },
+  ...props
+}) => (
+  <div className="flex align-items-center justify-content-center">
+    <Checkbox
+      inputId={field.name}
+      {...field}
+      {...props}
+      onChange={(e) => setFieldValue(field.name, e.checked)}
+      checked={field.value}
+    />
+    <label htmlFor={field.name} className="block text-900 font-medium mb-2">
+      {props.label}{" "}
+      {props.requiredlabel === "true" && <span className="text-danger">*</span>}
+    </label>
+    {Boolean(getIn(errors, field.name)) && getIn(touched, field.name) && (
+      <small className="text-danger">{getIn(errors, field.name)}</small>
+    )}
+  </div>
+);
---
+++ src/layout/PasswordChange.jsx
@@ -0,0 +1,50 @@
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+
+const PasswordChange = () => {
+  return (
+    <div className="flex align-items-center justify-content-center">
+      <div className="surface-card p-4 shadow-2 border-round w-full lg:w-3">
+        <div className="text-center mb-5">
+          <img
+            src="https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721898000&semt=sph"
+            alt="hyper"
+            height={50}
+            className="mb-3"
+          />
+          <div className="text-900 text-3xl font-medium mb-3">
+            Reset Password
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="email" className="block text-900 font-medium mb-2">
+            Password
+          </label>
+          <InputText
+            id="password"
+            type="password"
+            placeholder="Password"
+            className="w-full mb-3"
+            // onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <label htmlFor="password" className="block text-900 font-medium mb-2">
+            Confirm Password
+          </label>
+          <InputText
+            id="password"
+            type="password"
+            placeholder="Password"
+            className="w-full mb-3"
+            // onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button label="Sign In" className="w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordChange;
