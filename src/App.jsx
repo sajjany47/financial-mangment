afbcae404058053a1f53e1216d0c4b1436daf19e
QbAnurag <anurag.patra@qbent.in>
2024-08-23 11:46:13 +0530

    main.jsx and app.jsx page changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,93 @@ import CustomerList from "./pages/Admin/AddUser/CustomerList";
 import EmployeeList from "./pages/Admin/AddUser/EmployeeList";
 import AddEmployee from "./pages/Admin/AddUser/AddEmployee";
 import Branch from "./pages/Admin/Branch/Branch";
+import { useEffect } from "react";
+import { Instance } from "./shared/constant";
+import { RefreshToken } from "./layout/UserService";
+import {
+  ACCESS_TOKEN_STORAGE_KEY,
+  REFRESH_TOKEN_STORAGE_KEY,
+} from "./shared/Config";
+import Swal from "sweetalert2";
 
 function App() {
+  useEffect(() => {
+    const privateRequestInterceptor = Instance.interceptors.response.use(
+      (resposne) => {
+        return resposne;
+      },
+      async (error) => {
+        if (error?.response?.status === 403) {
+          try {
+            const result = await RefreshToken();
+
+            const accessToken = result.accessToken;
+            const refreshToken = result.refreshToken;
+            localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken);
+            localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken);
+
+            const prevRequest = error.config;
+            prevRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+            return Instance(prevRequest);
+          } catch (err) {
+            setTimeout(() => {
+              Swal.fire({
+                title: error.response.data.message,
+                icon: "error",
+              });
+            }, 350);
+
+            return Promise.reject(err);
+          }
+        }
+
+        Swal.fire({
+          title: error.response.data.message,
+          icon: "error",
+        });
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      Instance.interceptors.response.eject(privateRequestInterceptor);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Add a request interceptor
+    const publicRequestInterceptor = Instance.interceptors.request.use(
+      function (config) {
+        // Do something before request is sent
+        return config;
+      },
+      function (error) {
+        Swal.fire({
+          title: error.response.data.message,
+          icon: "error",
+        });
+        return Promise.reject(error);
+      }
+    );
+
+    const publicResponseInterceptor = Instance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        Swal.fire({
+          title: error.response.data.message,
+          icon: "error",
+        });
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      Instance.interceptors.response.eject(publicRequestInterceptor);
+      Instance.interceptors.response.eject(publicResponseInterceptor);
+    };
+  }, []);
+
   return (
     <>
       <BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -1,112 +1,21 @@
-import { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 import App from "./App.jsx";
 import "./index.css";
-import "../node_modules/primereact/resources/primereact.min.css";
-import "../node_modules/primereact/resources/themes/lara-light-cyan/theme.css";
 import "../node_modules/primeicons/primeicons.css";
 import "../node_modules/primeflex/primeflex.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
-import Swal from "sweetalert2";
-import { Instance } from "./shared/constant.js";
-import { RefreshToken } from "./layout/UserService.js";
-import {
-  ACCESS_TOKEN_STORAGE_KEY,
-  REFRESH_TOKEN_STORAGE_KEY,
-} from "./shared/Config.js";
-import axios from "axios";
 
-export const Root = () => {
-  useEffect(() => {
-    const privateRequestInterceptor = Instance.interceptors.response.use(
-      (resposne) => {
-        return resposne;
-      },
-      async (error) => {
-        if (error?.response?.status === 401) {
-          try {
-            const result = await RefreshToken();
-
-            const accessToken = result.accessToken;
-            const refreshToken = result.refreshToken;
-            localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken);
-            localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken);
-
-            const prevRequest = error.config;
-            prevRequest.headers["Authorization"] = `Bearer ${accessToken}`;
-            return axios(prevRequest);
-          } catch (err) {
-            setTimeout(() => {
-              Swal.fire({
-                title: error.message,
-                icon: "error",
-              });
-            }, 350);
-
-            return Promise.reject(err);
-          }
-        }
-
-        Swal.fire({
-          title: error.message,
-          icon: "error",
-        });
-        return Promise.reject(error);
-      }
-    );
-
-    return () => {
-      Instance.interceptors.response.eject(privateRequestInterceptor);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Add a request interceptor
-    const publicRequestInterceptor = Instance.interceptors.request.use(
-      function (config) {
-        // Do something before request is sent
-        return config;
-      },
-      function (error) {
-        Swal.fire({
-          title: error.message,
-          icon: "error",
-        });
-        return Promise.reject(error);
-      }
-    );
-
-    const publicResponseInterceptor = Instance.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      (error) => {
-        Swal.fire({
-          title: error.message,
-          icon: "error",
-        });
-        return Promise.reject(error);
-      }
-    );
-    return () => {
-      Instance.interceptors.response.eject(publicRequestInterceptor);
-      Instance.interceptors.response.eject(publicResponseInterceptor);
-    };
-  }, []);
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        {/* <React.StrictMode> */}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <React.StrictMode>
         <App />
-        {/* </React.StrictMode> */}
-      </PersistGate>
-    </Provider>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Root />);
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
+);
