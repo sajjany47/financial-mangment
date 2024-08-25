4980b969f6be7936214bf928bbef085fcbf26d46
sajjany47 <sajjany47@gmail.com>
2024-08-25 11:57:48 +0530

    chnages

--- src/axios/AxiosInstance.js
+++ src/axios/AxiosInstance.js
@@ -6,30 +6,48 @@ import {
   REFRESH_TOKEN_STORAGE_KEY,
 } from "../shared/Config";
 
+// Create a new Axios instance
 const axiosInstance = axios.create({
   baseURL: apiPath,
 });
 
+// Add an interceptor to handle 403 errors
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    if (error.response && error.response.status === 401) {
+    const originalRequest = error.config;
+
+    // Check if the error status is 403 and the request has not been retried
+    if (
+      error.response &&
+      error.response.status === 403 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true; // Prevent infinite loop
+
       try {
+        // Call the RefreshToken function to get a new access token
         const result = await RefreshToken();
 
         const accessToken = result.accessToken;
         const refreshToken = result.refreshToken;
+
+        // Store the new tokens in localStorage
         localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken);
         localStorage.setItem(REFRESH_TOKEN_STORAGE_KEY, refreshToken);
-        axios.defaults.headers.common["Authorization"] = `Bear ${accessToken}`;
 
-        const originalRequest = error.config;
-        originalRequest.headers["Authorization"] = `Bear ${accessToken}`;
-        return axios(originalRequest);
+        // Update the Authorization header with the new access token
+        originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+
+        // Retry the original request with the new token using the axiosInstance
+        return axiosInstance(originalRequest);
       } catch (refreshError) {
+        // If refreshing the token fails, reject the promise with the refreshError
         return Promise.reject(refreshError);
       }
     }
+
+    // If the error is not 403 or retry fails, reject the promise with the original error
     return Promise.reject(error);
   }
 );
