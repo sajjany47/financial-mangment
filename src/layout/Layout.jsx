ee08cd00125b690f3a1ba206673ad5a187c07091
QbAnurag <anurag.patra@qbent.in>
2024-09-03 15:48:05 +0530

    sidebar fixed

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,8 @@
-import { Outlet } from "react-router-dom";
 import SidebarLayout from "./SidebarLayout";
-import Topbar from "./Topbar";
 import { useDispatch, useSelector } from "react-redux";
 import Login from "./Login";
 import { Dialog } from "primereact/dialog";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PasswordChange from "./PasswordChange";
 import { setUser } from "../store/reducer/UserReducer";
 
@@ -12,11 +10,7 @@ const Layout = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user?.user);
   const [visible, setVisible] = useState(true);
-  const [collapsed, setCollapsed] = useState(false);
 
-  useEffect(() => {
-    setCollapsed(window.innerWidth > 540 ? false : true);
-  }, []);
   const dislogeClose = () => {
     setVisible(false);
     dispatch(
@@ -28,9 +22,6 @@ const Layout = () => {
     );
   };
 
-  const getCollapased = (e) => {
-    setCollapsed(e);
-  };
   return (
     <>
       {user.acccessToken ? (
@@ -50,20 +41,7 @@ const Layout = () => {
               <PasswordChange dislogeClose={dislogeClose} type="user" />
             </Dialog>
           ) : (
-            <div className="App">
-              <SidebarLayout getCollapased={getCollapased} />
-
-              <div
-                className="main-content"
-                style={{ flex: 1, marginLeft: collapsed ? "50px" : "250px" }}
-              >
-                <Topbar marginValue={collapsed ? "15px" : "1px"} />
-                {/* Add your main content here */}
-                <div style={{ marginLeft: collapsed ? "15px" : "1px" }}>
-                  <Outlet />
-                </div>
-              </div>
-            </div>
+            <SidebarLayout />
           )}
         </>
       ) : (
--- src/layout/SidebarLayout.jsx
+++ src/layout/SidebarLayout.jsx
@@ -3,10 +3,11 @@ import sidebarMenu from "./SidebarMenu";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Position } from "../shared/Config";
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { capitalizeFirstLetter } from "../shared/constant";
+import Topbar from "./Topbar";
 
-const SidebarLayout = (props) => {
+const SidebarLayout = () => {
   const user = useSelector((state) => state.user?.user);
   const [userMenu, setUserMenu] = useState([]);
   const [collapsed, setCollapsed] = useState(false);
@@ -40,56 +41,62 @@ const SidebarLayout = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div style={{ height: "100vh", display: "flex" }}>
-      <Sidebar
-        style={{ position: "fixed", height: "100%" }}
-        collapsed={collapsed}
-      >
-        <Menu>
-          <MenuItem
-            onClick={() => {
-              setCollapsed(!collapsed);
-              // eslint-disable-next-line react/prop-types
-              props.getCollapased(!collapsed);
-            }}
-            icon={<i className={"pi pi-bars"}></i>}
-            style={{ marginTop: "5px" }}
-          >
-            <h4>{capitalizeFirstLetter(user.data.position)}</h4>
-          </MenuItem>
-          {userMenu.map((item, index) => {
-            return item.submenu ? (
-              <SubMenu
-                label={item.title}
-                key={index}
-                icon={<i className={item.icon}></i>}
-                style={{ marginTop: "1px" }}
-              >
-                {item.submenu.map((res, ind) => {
-                  return (
-                    <MenuItem
-                      key={ind}
-                      icon={<i className={res.icon}></i>}
-                      component={<NavLink to={res.route} />}
-                    >
-                      {res.title}{" "}
-                    </MenuItem>
-                  );
-                })}
-              </SubMenu>
-            ) : (
-              <MenuItem
-                key={index}
-                icon={<i className={item.icon}></i>}
-                style={{ marginTop: "5px" }}
-                component={<NavLink to={item.route} />}
-              >
-                {item.title}
-              </MenuItem>
-            );
-          })}
-        </Menu>
-      </Sidebar>
+    <div>
+      <div style={{ display: "flex" }}>
+        <Sidebar
+          style={{ height: "100%" }}
+          className={` ${collapsed ? "sidebar collapsed" : "sidebar"}`}
+          collapsed={collapsed}
+        >
+          <Menu>
+            <MenuItem
+              onClick={() => {
+                setCollapsed(!collapsed);
+              }}
+              icon={<i className={"pi pi-bars"}></i>}
+              style={{ marginTop: "5px" }}
+            >
+              <h4>{capitalizeFirstLetter(user.data.position)}</h4>
+            </MenuItem>
+            {userMenu.map((item, index) => {
+              return item.submenu ? (
+                <SubMenu
+                  label={item.title}
+                  key={index}
+                  icon={<i className={item.icon}></i>}
+                  style={{ marginTop: "1px" }}
+                >
+                  {item.submenu.map((res, ind) => {
+                    return (
+                      <MenuItem
+                        key={ind}
+                        icon={<i className={res.icon}></i>}
+                        component={<NavLink to={res.route} />}
+                      >
+                        {res.title}{" "}
+                      </MenuItem>
+                    );
+                  })}
+                </SubMenu>
+              ) : (
+                <MenuItem
+                  key={index}
+                  icon={<i className={item.icon}></i>}
+                  style={{ marginTop: "5px" }}
+                  component={<NavLink to={item.route} />}
+                >
+                  {item.title}
+                </MenuItem>
+              );
+            })}
+          </Menu>
+        </Sidebar>
+
+        <main className="main-content">
+          <Topbar />
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
