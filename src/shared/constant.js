825fab2d8cca31f3fa3bf15886bff572f28cbd78
QbAnurag <anurag.patra@qbent.in>
2024-09-03 19:31:41 +0530

    country custom function create

--- src/shared/constant.js
+++ src/shared/constant.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN_STORAGE_KEY, REFRESH_TOKEN_STORAGE_KEY } from "./Config";
+import {
+  city,
+  countryList,
+  state,
+} from "../pages/Admin/AddUser/AddUserService";
 
 export const apiPath = import.meta.env.VITE_API_BASE_URL;
 export const env_path = import.meta.env.VITE_MODE;
@@ -48,4 +53,42 @@ export const headerWithFormData = () => {
 export const capitalizeFirstLetter = (str) => {
   if (!str) return str; // Return empty string if input is empty or null
   return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const fetchCountryStateCityData = async (data) => {
+  try {
+    const countryData = await countryList();
+    const response = {};
+    if (countryList) {
+      const filterCountry = countryData.data.find(
+        (item) => item.id === Number(data.country)
+      );
+
+      if (filterCountry) {
+        const item = await state(filterCountry.iso2);
+
+        response.state = item.data.map((item) => ({
+          label: item.name,
+          value: item.id,
+        }));
+
+        const filterState = item.data.find(
+          (stateItem) => stateItem.id === Number(data.state)
+        );
+
+        if (filterState) {
+          const elm = await city(filterCountry.iso2, filterState.iso2);
+
+          response.city = elm.data.map((item) => ({
+            label: item.name,
+            value: item.id,
+          }));
+        }
+      }
+
+      return response;
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 };
