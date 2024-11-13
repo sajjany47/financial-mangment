31ee1191f8b8b478a6a0996b35d64749193e5f55
QbAnurag <anurag.patra@qbent.in>
2024-11-13 12:13:45 +0530

    invester

--- src/pages/Admin/Finance/InvestorViewDetails.jsx
+++ src/pages/Admin/Finance/InvestorViewDetails.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { getInvestorDetails } from "./FinanceService";
+import { useParams } from "react-router-dom";
+import Loader from "../../../component/Loader";
+
 const InvestorViewDetails = () => {
-  return <div>InvestorViewDetails</div>;
+  const id = useParams().id;
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    getInvestorDetails(id)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(data);
+  return <>{loading && <Loader />}</>;
 };
 
 export default InvestorViewDetails;
